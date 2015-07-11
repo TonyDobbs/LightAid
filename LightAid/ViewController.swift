646b64cdc30c38956c27c7caf10f9c331f08ff45
Tony Dobbs <dobbs@anthonydobbs.com>
2015-07-11 11:56:30 -0700

    App Functional

--- LightAid/ViewController.swift
+++ LightAid/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBAction func beginTherapy(sender: AnyObject) {
+        therapyView.hidden=false;
+    }
 
+    @IBOutlet weak var therapyView: UIView!
+    
+    @IBOutlet var cancelTherapy: UITapGestureRecognizer!
+    
+    @IBAction func endTherapy(sender: AnyObject) {
+        therapyView.hidden=true;
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        therapyView.hidden=true;
     }
 
     override func didReceiveMemoryWarning() {
